60a9db140386992bba025f232f62805e0d0f6376
YGao2005 <41841169+YGao2005@users.noreply.github.com>
2025-05-02 23:42:33 -0700

    Initial Commit

---
+++ uclamoods/uclamoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  uclamoodsApp.swift
+//  uclamoods
+//
+//  Created by Yang Gao on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct uclamoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
