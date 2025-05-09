9d5a5327ee54a22e054315b543cbe257584d55c8
dsun05 <131048273+dsun05@users.noreply.github.com>
2025-05-08 20:34:36 -0700

    Delete server/src directory

--- server/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import helloRoutes from './hello.routes';
-
-const router = express.Router();
-
-// API routes
-router.use('/hello', helloRoutes);
-
-export default router;
