0958f1145a9d9b59143bb51a524fdd68b912ea49
DiAndyW <fangwanggo@outlook.com>
2025-05-10 18:01:37 -0700

    Added SignInView content

---
+++ uclamoods/View/SignInView/SignInView.swift
@@ -0,0 +1,112 @@
+//
+//  SignInView.swift
+//  uclamoods
+//
+//  Created by Di Xuan Wang on 5/10/25.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    @EnvironmentObject private var router: MoodAppRouter
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack{
+                Color.black
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack(alignment: .center, spacing: 15) {
+                    Text("Morii")
+                        .font(.custom("Georgia", size: 60))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(alignment: .center)
+                    Text("Log In")
+                        .font(.custom("Georgia", size: 50))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+            
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Email Address")
+                            .font(.custom("Georgia", size: 20))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(alignment: .leading)
+                        TextField("Required",text: $username)
+                            .textInputAutocapitalization(.never)
+                            .disableAutocorrection(true)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.white.opacity(0.1))
+                            .cornerRadius(20)
+                            .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.white.opacity(0.25), lineWidth: 1))
+                            .font(.custom("Georgia", size: 20).weight(.bold))
+                            .frame(width: 300)
+                        
+                        Text("Password")
+                            .font(.custom("Georgia", size: 20))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(alignment: .leading)
+                        SecureField("Required",text: $password)
+                            .textInputAutocapitalization(.never)
+                            .disableAutocorrection(true)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.white.opacity(0.1))
+                            .cornerRadius(20)
+                            .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.white.opacity(0.25), lineWidth: 1))
+                            .font(.custom("Georgia", size: 20))
+                            .frame(width: 300)
+                        
+                        Button(action: {
+                            print("Forgot password tapped")
+                        }) {
+                            Text("Forgot Password?")
+                                .font(.custom("Georgia", size: 16))
+                                .foregroundColor(.white)
+                                .underline()
+                        }
+                    }
+                    
+                    
+                    Button(action: {
+                        let feedback = UIImpactFeedbackGenerator(style: .medium)
+                        feedback.prepare()
+                        feedback.impactOccurred()
+                        
+//                        router.navigate(to: .home)
+                    }) {
+                        Text("Log In")
+                            .font(.custom("Georgia", size: 20))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(width: 300, height: 60)
+                            .background(Color.white.opacity(0.1))
+                            .cornerRadius(20)
+                            .shadow(radius: 5)
+                    }
+                    
+                
+                }
+            
+            }
+        }
+        
+    }
+    
+//    Text("Check-in Complete")
+//        .font(.custom("Georgia", size: 28))
+//        .fontWeight(.bold)
+//        .foregroundColor(.white)
+}
+
+#Preview {
+    SignInView()
+        .environmentObject(MoodAppRouter())
+}
