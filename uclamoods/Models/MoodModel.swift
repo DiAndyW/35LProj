0cba10fef1827b41eea6dfeec05e7227ea1ea71c
DiAndyW <fangwanggo@outlook.com>
2025-05-22 22:09:15 -0700

    Friends page now shows friends list, which has the proper dataflow. Page loading gets data from the view model, which in turn gets the data from the service, which will fetch from the backend

---
+++ uclamoods/Models/MoodModel.swift
@@ -0,0 +1,15 @@
+//
+//  MoodModel.swift
+//  uclamoods
+//
+//  Created by Di Xuan Wang on 5/22/25.
+//
+
+import SwiftUI
+
+//temporary
+struct Mood {
+    let name: String
+    let imageName: String
+    let color: Color
+}
--- uclamoods/View/FriendsView/FriendsView.swift
+++ uclamoods/View/FriendsView/FriendsView.swift
@@ -9,16 +9,41 @@ import SwiftUI
 
 struct FriendsView: View {
     @EnvironmentObject private var router: MoodAppRouter
+    @StateObject private var viewModel = FriendsViewModel()
     
     var body: some View {
         ZStack{
             Color.black
                 .edgesIgnoringSafeArea(.all)
             VStack(spacing: 0) {
+                
+                //header
                 VStack(alignment: .center, spacing:15) {
-                    Text("This is the friends page!")
-                        .font(.custom("Georgia", size:24))
+                    Text("Friends!")
+                        .font(.custom("Georgia", size:28))
                         .foregroundColor(.white)
+                        .fontWeight(.bold)
+                }
+                .padding(.top, 20)
+                .padding(.horizontal)
+                
+                //friends list
+                if viewModel.isLoading {
+                    Spacer()
+                    ProgressView("Loading friends...")
+                        .foregroundColor(.white)
+                    Spacer()
+                } else {
+                    ScrollView {
+                        LazyVStack(spacing: 12) {
+                            ForEach(viewModel.friends) { friend in
+                                FriendCard(friend: friend)
+                            }
+                        }
+                        .padding(.horizontal, 16)
+                        .padding(.top, 20)
+                        .padding(.bottom, 100)
+                    }
                 }
                     
                 Spacer()
@@ -31,6 +56,14 @@ struct FriendsView: View {
             
         }
         .edgesIgnoringSafeArea(.bottom)
+        .onAppear {
+            viewModel.loadFriends()
+        }
+        .alert("Error", isPresented: .constant(viewModel.errorMessage != nil)) {
+            Button("OK") { viewModel.errorMessage = nil }
+        } message: {
+            Text(viewModel.errorMessage ?? "")
+        }
     }
 }
 
